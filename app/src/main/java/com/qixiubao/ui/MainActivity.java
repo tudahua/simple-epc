dde91ebc8f6bd70122bce3cf23e66c7b707b4134
tudahua <543582461@qq.com>
2020-04-01 10:05:07 +0800

    增加vin字段

--- app/src/main/java/com/qixiubao/ui/MainActivity.java
+++ app/src/main/java/com/qixiubao/ui/MainActivity.java
@@ -1,9 +1,11 @@
 package com.qixiubao.ui;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -12,6 +14,7 @@ import com.simpleepc.api.ApiServiceDelegate;
 import com.simpleepc.model.RequireData;
 import com.simpleepc.model.SimpleEpcDetail;
 import com.simpleepc.ui.SimpleEpcCatalogActivity;
+import com.simpleepc.utils.StatusBarUtil;
 
 /**
  * @author tudahua
@@ -23,18 +26,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        StatusBarUtil.setStatusBarColor(this, Color.WHITE);
+        StatusBarUtil.StatusBarLightMode(this);
         setContentView(R.layout.activity_main);
         //初始化appId和secret
         ApiServiceDelegate.init(this, "53652a26d4a8db783b5b0a7736e23033", "839f3235ba41946d8a3ee33bce4124ea", true);
         final RequireData requireData = new RequireData();
         //0:BSM 模式 1：EPC模式
-        requireData.type = 1;
+        requireData.type = 0;
         requireData.brand_id = 14;
         requireData.model_id = 7252;
         requireData.epc_id = 1014;
-        requireData.token = "0768036056aa96bbeebe534669dfea2e";
-        requireData.access_time = "1561687126";
-        requireData.param = "Krf75KRAXZ00Uwa0aJy9jjJzbneuYdIeaNs6YZy9zvqxyuq-_ucZ_jz7z3k4UdC_V23zlJy-gJz7z3GxZdAebdokzvRziFixkz0q";
+        requireData.token = "4a55c0670f02c0b6ce220e8f837238d4";
+        requireData.access_time = "1585641037";
+        requireData.param = "LKL450T6XZ00Uwa0aJy9jjJzbneuYdIeaNs6YZy9zvqxyuq-_ucZ_jz7z3k4UdC_V23zlJy-gJz7z3GxZdAebdokzvRziFizjZ0q";
         findViewById(R.id.default_ui_tv).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -56,9 +61,9 @@ public class MainActivity extends AppCompatActivity {
                 SimpleEpcDetail.DataBean.ListBean listBean = data.data.list.get(position);
                 Toast.makeText(MainActivity.this,
                         "onSelected=" + listBean.caption, Toast.LENGTH_LONG).show();
+                Log.d("Main", "onSelected=" + listBean.caption);
             }
         });
-
 
     }
 }
